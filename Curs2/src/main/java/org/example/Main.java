a5021aafbc861627089d01433d79bc6d9b3196c5
Sebastian <75882476+sebisleam33@users.noreply.github.com>
2024-11-13 11:46:24 +0200

    Add files via upload

--- Curs2/src/main/java/org/example/Main.java
+++ Curs2/src/main/java/org/example/Main.java
@@ -1,16 +1,18 @@
 package org.example;
 
 public class Main {
-    byte x = 10;
-    byte y = 20;
-    byte z = (byte) (x + y);
+    public void operatorsTest() {
+        int count = 10;
+        int extracount = 2;
+        int totalcount = count;
+        if(extracount == 1 ^ count < 100){
+            totalcount = count + extracount;
+        }
+        System.out.println(totalcount);
+    }
     public static void main(String[] args) {
         Main obj = new Main();
-        System.out.println(obj.z);
+        obj.operatorsTest();
     }
 
-    public void scopeTest () {
-        String text = "Welcome";
-        System.out.println(text);
-    }
 }
