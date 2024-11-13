189a0587bc7db376adb6b2f39b5088a4abb7c297
Sebastian <75882476+sebisleam33@users.noreply.github.com>
2024-11-13 11:25:04 +0200

    Add files via upload

--- Curs2/src/main/java/org/example/Main.java
+++ Curs2/src/main/java/org/example/Main.java
@@ -1,9 +1,16 @@
 package org.example;
 
 public class Main {
-    int x;
+    byte x = 10;
+    byte y = 20;
+    byte z = (byte) (x + y);
     public static void main(String[] args) {
         Main obj = new Main();
-        System.out.println(obj.x);
+        System.out.println(obj.z);
+    }
+
+    public void scopeTest () {
+        String text = "Welcome";
+        System.out.println(text);
     }
 }
