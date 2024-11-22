f330aa4ed4ecea9e6e277837f2c6654edd6580d0
Sebastian <75882476+sebisleam33@users.noreply.github.com>
2024-11-22 12:13:14 +0200

    Add files via upload

--- Curs2/src/main/java/org/example/Main.java
+++ Curs2/src/main/java/org/example/Main.java
@@ -1,18 +1,13 @@
 package org.example;
 
 public class Main {
-    public void operatorsTest() {
-        int count = 10;
-        int extracount = 2;
-        int totalcount = count;
-        if(extracount == 1 ^ count < 100){
-            totalcount = count + extracount;
-        }
-        System.out.println(totalcount);
-    }
     public static void main(String[] args) {
         Main obj = new Main();
-        obj.operatorsTest();
+        obj.sayHello("Male");
     }
 
+    public void sayHello(String gender) {
+        String newText = gender.equals("Male") ? "Hello, Sir" : "Hello Madam";
+        System.out.println(newText);
+    }
 }
