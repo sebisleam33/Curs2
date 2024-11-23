0973e83aa29a69dd75e7799b9f0b814ab904c78f
Sebastian <75882476+sebisleam33@users.noreply.github.com>
2024-11-23 17:29:16 +0200

    Add files via upload

--- Curs2/src/main/java/org/example/FlowControlPckg/FlowControlSample.java
+++ Curs2/src/main/java/org/example/FlowControlPckg/FlowControlSample.java
@@ -2,33 +2,9 @@ package org.example.FlowControlPckg;
 
 public class FlowControlSample {
     public static void main(String[] args) {
-        int guess = 5;
-        switch (guess) {
-            case 1 : {
-                System.out.println("Guess is 1.");
-                break;
-            }
-            case 2 : {
-                System.out.println("Guess is 2.");
-                break;
-            }
-            case 3 : {
-                System.out.println("Guess is 3.");
-                break;
-            }
-            default : {
-                System.out.println("Guess is greater than 3 or less than 1.");
-                break;
-            }
+        int[] intArray = {2,4,6,1,3};
+        for (int i : intArray) {
+            System.out.println(i);
         }
-//        if (guess == 1 ) {
-//            System.out.println("Guess is 1");
-//        } else if (guess == 2 ) {
-//            System.out.println("Guess is 2");
-//        } else if (guess == 3 ) {
-//            System.out.println("Guess is 3 ");
-//        } else {
-//            System.out.println("Guess is greater than 3 or less than 1.");
-//        }
     }
 }
