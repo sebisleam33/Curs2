e3aeccfe1231bdb1ad27b6c76333eecaca6f5211
Sebastian <75882476+sebisleam33@users.noreply.github.com>
2024-11-23 17:11:44 +0200

    Add files via upload

--- Curs2/src/main/java/org/example/FlowControlPckg/FlowControlSample.java
+++ Curs2/src/main/java/org/example/FlowControlPckg/FlowControlSample.java
@@ -3,14 +3,32 @@ package org.example.FlowControlPckg;
 public class FlowControlSample {
     public static void main(String[] args) {
         int guess = 5;
-        if (guess == 1 ) {
-            System.out.println("Guess is 1");
-        } else if (guess == 2 ) {
-            System.out.println("Guess is 2");
-        } else if (guess == 3 ) {
-            System.out.println("Guess is 3 ");
-        } else {
-            System.out.println("Guess is greater than 3 or less than 1.");
+        switch (guess) {
+            case 1 : {
+                System.out.println("Guess is 1.");
+                break;
+            }
+            case 2 : {
+                System.out.println("Guess is 2.");
+                break;
+            }
+            case 3 : {
+                System.out.println("Guess is 3.");
+                break;
+            }
+            default : {
+                System.out.println("Guess is greater than 3 or less than 1.");
+                break;
+            }
         }
+//        if (guess == 1 ) {
+//            System.out.println("Guess is 1");
+//        } else if (guess == 2 ) {
+//            System.out.println("Guess is 2");
+//        } else if (guess == 3 ) {
+//            System.out.println("Guess is 3 ");
+//        } else {
+//            System.out.println("Guess is greater than 3 or less than 1.");
+//        }
     }
 }
