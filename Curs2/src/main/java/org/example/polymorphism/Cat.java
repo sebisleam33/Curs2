6f2bb6c698da8f23b7880cf55b29ca9dc542f656
Sebastian <75882476+sebisleam33@users.noreply.github.com>
2024-11-29 09:40:53 +0200

    Add files via upload

--- Curs2/src/main/java/org/example/polymorphism/Cat.java
+++ Curs2/src/main/java/org/example/polymorphism/Cat.java
@@ -8,10 +8,19 @@ public class Cat extends DomesticAnimal{
         System.out.println("I will eat meat as well.");
     }
 
+    public void run(boolean hasDanger) {
+        if(hasDanger) {
+            System.out.println("I'm running at: 48 KM/H");
+        } else {
+            super.run();
+        }
+    }
+
     public static void main(String[] args) {
         Cat c = new Cat();
         c.setMinSpeed("34 KM/H");
         c.eat();
         c.run();
+        c.run(true);
     }
 }
