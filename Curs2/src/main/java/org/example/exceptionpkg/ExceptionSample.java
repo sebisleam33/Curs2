96719f95d018dbd986a7695c124c2b569fb3a762
Sebastian <75882476+sebisleam33@users.noreply.github.com>
2024-12-05 12:44:37 +0200

    Add files via upload

--- Curs2/src/main/java/org/example/exceptionpkg/ExceptionSample.java
+++ Curs2/src/main/java/org/example/exceptionpkg/ExceptionSample.java
@@ -2,7 +2,12 @@ package org.example.exceptionpkg;
 
 public class ExceptionSample {
     public static void main(String[] args) {
-        int userInput = 1;
+        int userInput = 0;
+        try {
+            userInput = Integer.parseInt("abc");
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid numbers.");
+        }
         String fruits[] = {"Bananas", "Watermelons", "Apples", "Oranges"};
         System.out.println("User picked: " + fruits[userInput - 1]);
     }
