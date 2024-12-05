a645a4d4f9e3835f152dd113c37e6639a817632a
Sebastian <75882476+sebisleam33@users.noreply.github.com>
2024-12-05 12:49:24 +0200

    Add files via upload

--- Curs2/src/main/java/org/example/exceptionpkg/ExceptionSample.java
+++ Curs2/src/main/java/org/example/exceptionpkg/ExceptionSample.java
@@ -4,11 +4,15 @@ public class ExceptionSample {
     public static void main(String[] args) {
         int userInput = 0;
         try {
-            userInput = Integer.parseInt("abc");
+            userInput = Integer.parseInt("8");
+            String fruits[] = {"Bananas", "Watermelons", "Apples", "Oranges"};
+            System.out.println("User picked: " + fruits[userInput - 1]);
         } catch (NumberFormatException e) {
             System.out.println("Invalid numbers.");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Give numbers between 1 to 4.");
+        } catch (Exception e) {
+            System.out.println("Something went wrong.");
         }
-        String fruits[] = {"Bananas", "Watermelons", "Apples", "Oranges"};
-        System.out.println("User picked: " + fruits[userInput - 1]);
     }
 }
