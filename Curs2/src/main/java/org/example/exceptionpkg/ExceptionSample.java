bd5557f43dd0b071f33606b39ab93058360eb451
Sebastian <75882476+sebisleam33@users.noreply.github.com>
2024-12-05 12:39:15 +0200

    Add files via upload

---
+++ Curs2/src/main/java/org/example/exceptionpkg/ExceptionSample.java
@@ -0,0 +1,9 @@
+package org.example.exceptionpkg;
+
+public class ExceptionSample {
+    public static void main(String[] args) {
+        int userInput = 1;
+        String fruits[] = {"Bananas", "Watermelons", "Apples", "Oranges"};
+        System.out.println("User picked: " + fruits[userInput - 1]);
+    }
+}
